46b0ade5deeb0dcce0329ddecb269036ede3c4f6
TOKY <tokyrakotoarivony5@gmail.com>
2022-12-06 17:04:38 +0300

    commit

--- src/server/Pane.java
+++ src/server/Pane.java
@@ -17,7 +17,7 @@ public class Pane extends JPanel {
             Graphics2D g2d = (Graphics2D) g;
             g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
             g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-            g2d.drawImage(buffImg, 0, 0, buffImg.getWidth(null), buffImg.getHeight(null),null);
+            g2d.drawImage(buffImg, 0, 0, this);
 
         } catch (Exception e) {
             e.printStackTrace();
